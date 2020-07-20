7af449399f62a4f1eea081c09661f5b97bac6664
Dylan SINAULT <dylan.sinault@exakis-nelite.com>
2020-07-20 17:21:42 +0200

    Update link

--- ARM Template/Storage/StorageAccount/deploy.ps1
+++ ARM Template/Storage/StorageAccount/deploy.ps1
@@ -1,5 +1,5 @@
 Select-AzSubscription -SubscriptionName "Lyon-IA-ouverte"
 
 New-AzResourceGroupDeployment -ResourceGroupName WEUDSTRSGZZZF00 `
--TemplateUri "https://raw.githubusercontent.com/SINAULT/DevOpsRepo/master/ARM%20Template/Storage/StorageAccount/template.json" `
--TemplateParameterUri "https://raw.githubusercontent.com/SINAULT/DevOpsRepo/master/ARM%20Template/Storage/StorageAccount/parameters.json"
+-TemplateUri "https://raw.githubusercontent.com/SINAULT/DevOpsRepo/master/ARM%20Template/Storage/StorageAccount/azuredeploy.json" `
+-TemplateParameterUri "https://raw.githubusercontent.com/SINAULT/DevOpsRepo/master/ARM%20Template/Storage/StorageAccount/azuredeploy.parameters.json"
