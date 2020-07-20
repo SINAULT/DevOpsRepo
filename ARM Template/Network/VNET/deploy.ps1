4c9ecee9ef5d5163ba5d6beea9e98e504c43ed1b
Dylan SINAULT <dylan.sinault@exakis-nelite.com>
2020-07-20 16:35:05 +0200

    Update Network files

--- ARM Template/Network/VNET/deploy.ps1
+++ ARM Template/Network/VNET/deploy.ps1
@@ -1,5 +1,5 @@
 Select-AzSubscription -SubscriptionName "Lyon-IA-ouverte"
 
 New-AzResourceGroupDeployment -ResourceGroupName WEUDSTRSGZZZF00 `
--TemplateUri "https://raw.githubusercontent.com/SINAULT/DevOpsRepo/master/ARM%20Template/Network/VNET/template.json" `
--TemplateParameterUri "https://raw.githubusercontent.com/SINAULT/DevOpsRepo/master/ARM%20Template/Network/VNET/parameters.json"
+-TemplateUri "https://raw.githubusercontent.com/SINAULT/DevOpsRepo/master/ARM%20Template/Network/VNET/azuredeploy.json" `
+-TemplateParameterUri "https://raw.githubusercontent.com/SINAULT/DevOpsRepo/master/ARM%20Template/Network/VNET/azuredeploy.parameters.json"
