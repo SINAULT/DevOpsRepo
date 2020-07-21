00ddcf0d99b99ee9b721fffc5dd51ce8ddabb77a
Dylan SINAULT <dylan.sinault@exakis-nelite.com>
2020-07-21 10:28:22 +0200

    Update VM Ubuntu

--- ARM Template/VMs/Ubuntu 18.04 LTS/deploy.ps1
+++ ARM Template/VMs/Ubuntu 18.04 LTS/deploy.ps1
@@ -1,6 +1,5 @@
 Select-AzSubscription -SubscriptionName "Lyon-IA-ouverte"
 
 New-AzResourceGroupDeployment -ResourceGroupName WEUDSTRSGZZZF00 `
--Prefix "00" `
--TemplateUri "https://raw.githubusercontent.com/SINAULT/DevOpsRepo/master/ARM%20Template/VMs/Ubuntu%2018.04%20LTS/template.json" `
--TemplateParameterUri "https://raw.githubusercontent.com/SINAULT/DevOpsRepo/master/ARM%20Template/VMs/Ubuntu%2018.04%20LTS/parameters.json"
+-TemplateUri "https://raw.githubusercontent.com/SINAULT/DevOpsRepo/master/ARM%20Template/VMs/Ubuntu%2018.04%20LTS/azuredeploy.json" `
+-TemplateParameterUri "https://raw.githubusercontent.com/SINAULT/DevOpsRepo/master/ARM%20Template/VMs/Ubuntu%2018.04%20LTS/azuredeploy.parameters.json"
