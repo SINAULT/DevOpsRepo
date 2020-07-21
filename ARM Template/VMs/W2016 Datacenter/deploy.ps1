be956321648795fa90b49f8b8a114eeaac839d7b
Dylan SINAULT <dylan.sinault@exakis-nelite.com>
2020-07-21 11:49:24 +0200

    Update Windows VM

--- ARM Template/VMs/W2016 Datacenter/deploy.ps1
+++ ARM Template/VMs/W2016 Datacenter/deploy.ps1
@@ -1,6 +1,5 @@
 Select-AzSubscription -SubscriptionName "Lyon-IA-ouverte"
 
 New-AzResourceGroupDeployment -ResourceGroupName WEURSGDSTZZZF00 `
--Prefix "00" `
--TemplateUri "https://raw.githubusercontent.com/SINAULT/DevOpsRepo/master/ARM%20Template/VMs/W2016%20Datacenter/template.json" `
--TemplateParameterUri "https://raw.githubusercontent.com/SINAULT/DevOpsRepo/master/ARM%20Template/VMs/W2016%20Datacenter/parameters.json"
+-TemplateUri "https://raw.githubusercontent.com/SINAULT/DevOpsRepo/master/ARM%20Template/VMs/W2016%20Datacenter/azuredeploy.json" `
+-TemplateParameterUri "https://raw.githubusercontent.com/SINAULT/DevOpsRepo/master/ARM%20Template/VMs/W2016%20Datacenter/azure.parameters.json"
