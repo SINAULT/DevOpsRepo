4730bc9769bda739f11fcfde2c2a4000d5933eb9
Dylan SINAULT <dylan.sinault@exakis-nelite.com>
2020-07-23 10:02:24 +0200

    Update PS script

--- ARM Template/AKS/deploy.ps1
+++ ARM Template/AKS/deploy.ps1
@@ -1,3 +1,5 @@
 Select-AzSubscription -SubscriptionName "Lyon-IA-ouverte"
 
-New-AzResourceGroupDeployment -ResourceGroupName WEUDSTRSGZZZF00 -TemplateFile "./template.json" -TemplateParameterFile "./parameters.json"
+New-AzResourceGroupDeployment -ResourceGroupName WEUDSTRSGZZZF00 `
+-TemplateUri "https://raw.githubusercontent.com/SINAULT/DevOpsRepo/master/ARM%20Template/AKS/azuredeploy.json" `
+-TemplateParameterUri "https://raw.githubusercontent.com/SINAULT/DevOpsRepo/master/ARM%20Template/AKS/azuredeploy.parameters.json"
