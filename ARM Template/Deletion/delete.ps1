3a0b3c8b751bd693db9019db97480e146c01a913
Dylan SINAULT <dylan.sinault@exakis-nelite.com>
2020-07-28 15:26:49 +0200

    Update in "Uri"

--- ARM Template/Deletion/delete.ps1
+++ ARM Template/Deletion/delete.ps1
@@ -1,5 +1,5 @@
 Select-AzSubscription -SubscriptionName "Lyon-IA-ouverte"
 
 New-AzResourceGroupDeployment -ResourceGroupName WEUDSTRSGZZZF00 `
--TemplateFile "https://raw.githubusercontent.com/SINAULT/DevOpsRepo/master/ARM%20Template/Deletion/template.json" `
+-TemplateUri "https://raw.githubusercontent.com/SINAULT/DevOpsRepo/master/ARM%20Template/Deletion/template.json" `
 -Verbose -Mode Complete -Force
